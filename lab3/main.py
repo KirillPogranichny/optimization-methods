f381b6d8a715b8936084c8d615ba86bdb6dd5199
KirillPogranichny <kirillpogranichny@gmail.com>
2023-06-05 00:43:57 +1000

    lab3 fix and latex file done

--- lab3/main.py
+++ lab3/main.py
@@ -39,14 +39,6 @@ class Solution:
                 answer.append(0)
         print("Промежуточное решение:\n", np.array(answer))
 
-    def __create_main(self, c, b):
-        main_mat = np.zeros((9, 15))
-        main_mat[0][:c.shape[0]] = -c
-        main_mat[1:, :6] = self.__A
-        main_mat[1:, 6: 14] = np.eye(8)
-        main_mat[1:, 14] = b
-        return main_mat
-
     def __create_support_mat(self):
         main_mat = np.zeros((7, 21))
         main_mat[1:, :8] = self.__A.T
@@ -61,17 +53,6 @@ class Solution:
         main_mat[0] -= temp
 
         return main_mat
-
-    def __to_ready_double_problem(self, mat, b):
-        mat[0, :8] = b
-        mat[0, 14] = 0
-        for i in range(15):
-            if sum(mat[1:, i]) == 1 and mat[0, i] != 0:
-                index_line = np.where(mat[1:, i] == 1)[0][0] + 1
-                mat[0, :15] -= mat[index_line] * mat[0][i]
-                mat[0][i] = 0
-
-        return mat
 
     def __simplex_method(self, main_mat, m, n):
         t = 0
@@ -93,26 +74,48 @@ class Solution:
             for i in range(len(cur_col)):
                 if cur_col[i] != 1:
                     main_mat[i] -= main_mat[index_min_line] * cur_col[i]
-
+        t += 1
         print("END OF SIMPLEX!!!: \n")
         self.__fun_for_print(main_mat, index_min_line, index_min_col, t)
 
         return main_mat
 
+    def __create_main(self, c, b):
+        main_mat = np.zeros((9, 15))
+        main_mat[0][:c.shape[0]] = -c
+        main_mat[1:, :6] = self.__A
+        main_mat[1:, 6: 14] = np.eye(8)
+        main_mat[1:, 14] = b
+        return main_mat
+
+    def __to_ready_double_problem(self, mat, b):
+        mat[0, :8] = b
+        mat[0, 14] = 0
+        for i in range(15):
+            if sum(mat[1:, i]) == 1 and mat[0, i] != 0:
+                index_line = np.where(mat[1:, i] == 1)[0][0] + 1
+                mat[0, :15] -= mat[index_line] * mat[0][i]
+                mat[0][i] = 0
+
+        return mat
+
     def execute(self):
+        print("---Прямая---")
+        print(tabulate(self.__simplex_method(self.__create_main(self.__c, self.__b), m=9, n=15),
+                       tablefmt="latex", floatfmt=".2f"))
+
         support_mat = self.__simplex_method(self.__create_support_mat(), m=7, n=21)
-        print("LAST STEP OF DOUBLE PROBLEM PREPARE:\n")
+        print("Последний шаг подготовки к решению двойственной задачи:\n")
         print(tabulate(support_mat, tablefmt="latex", floatfmt=".2f"))
 
         double_mat = np.column_stack([support_mat[0:, :14], support_mat[0:, 20]])
         double_mat = self.__to_ready_double_problem(double_mat, self.__b)
 
-        print("DOUBLE MATRIX: \n")
+        print("---DOUBLE MATRIX---\n")
         print(tabulate(double_mat, tablefmt="latex", floatfmt=".2f"))
 
-        print("Прямая\n", tabulate(self.__simplex_method(self.__create_main(self.__c, self.__b), m=9, n=15),
-                                   tablefmt="latex", floatfmt=".2f"))
-        print("Двойственная\n", tabulate(self.__simplex_method(double_mat, m=7, n=15), tablefmt="latex", floatfmt=".2f"))
+        print("---Двойственная---\n")
+        print(tabulate(self.__simplex_method(double_mat, m=7, n=15), tablefmt="latex", floatfmt=".2f"))
 
 
 if __name__ == "__main__":
